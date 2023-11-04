004a419cb93bfd4b05f93940f73dfe43b1f3e065
Akarsh Velan <akarsh.velan@gmail.com>
2023-11-04 22:42:20 +0530

    Code Shortened.

--- Random Password Generator.py
+++ Random Password Generator.py
@@ -1,5 +1,5 @@
 import random
-choices_y = ('yes', 'ye', 'y', 's')
+ycho = ('yes', 'ye', 'y', 's')
 def savefile(l):
     save = open('svdpswd.txt', 'a+')
     xa = '• '
@@ -11,30 +11,30 @@ def main():
     ch0 = int(input('Enter your choice? (1/2/3) '))
     if ch0 == 1:
         lg = int(input("Enter the required length of the generated password:  "))
-        ch1 = input("Do you want to include numbers? (y/n)").lower()
+        ch1 = input("Include Numbers? (y/n)").lower()
         l = []
         alp = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
         def options(l):
-            ch2 = input('Do you want to edit or remove a character in this password? (y/n) ').lower()
-            if ch2 in choices_y:
+            ch2 = input('Edit or Remove a character in this password? (y/n) ').lower()
+            if ch2 in ycho:
                 ch3 = input('Do you want to remove a character? (y/n) ').lower();print()
                 print('Current Password: ', ''.join(l));print()
-                if ch3 in choices_y:
+                if ch3 in ycho:
                     ch4 = int(input('Enter character number you want to remove? (Ex. 1,3,7) '))
                     del l[ch4 - 1];print()
                     print('Generated Password: ', "".join(l));print()
                     sv = input('Do you want to save your password: (y/n) ')
-                    if sv in choices_y:
+                    if sv in ycho:
                         savefile(l)
                     print()
                     ch11 = input('Do you want to create a new password (y/n): ').lower()
-                    if ch11 in choices_y:
+                    if ch11 in ycho:
                         main()
                     else:
                         print()
                         ch5 = input('Do you wanna go back to the previous options? (y/n) ').lower()
                         print()
-                        if ch5 in choices_y:
+                        if ch5 in ycho:
                             options(l)
                         else:
                             print('-----' * 25);main()
@@ -46,17 +46,17 @@ def main():
                     l.insert(ind, ch6);print()
                     print('Generated Password: ', "".join(l));print()
                     sv = input('Do you want to save your password: (y/n) ')
-                    if sv in choices_y:
+                    if sv in ycho:
                         savefile(l)
                     print()
                     ch12 = input('Do you want to create a new password: (y/n) ').lower()
-                    if ch12 in choices_y:
+                    if ch12 in ycho:
                         main()
                     else:
                         print()
                         ch7 = input('Do you wanna go back to the previous options? (y/n) ').lower()
                         print()
-                        if ch7 in choices_y:
+                        if ch7 in ycho:
                             options(l)
                         else:
                             print('-----' * 25);main()
@@ -65,22 +65,22 @@ def main():
         def generate(l):
             print()
             print('Generated Password: ', "".join(l));print()
-            c = input('Do you want to edit or remove a charcter in the generated password? (y/n) ')
-            if c in choices_y:
+            c = input('Edit or remove a charcter in the generated password? (y/n) ')
+            if c in ycho:
                 options(l)
             sv = input('Do you want to save your password: (y/n) ')
-            if sv in choices_y:
+            if sv in ycho:
                 savefile(l)
             print()
             ch13 = input('Do you want to create a new password: (y/n) ').lower()
-            if ch13 in choices_y:
+            if ch13 in ycho:
                 main()
             else:
                 print();options(l)
         def start(l):
             l.clear()
             i = 0
-            if ch1 in choices_y:
+            if ch1 in ycho:
                 while i != lg:
                     tmp1 = random.choice(alp)
                     tmp2 = str(random.randint(0, 9))
@@ -94,20 +94,20 @@ def main():
                 if i == lg:
                     print()
                     print('Generated Password: ', "".join(l));print()
-                    d = input('Do you want to edit or remove a charcter in the generated password? (y/n) ')
-                    if d in choices_y:
+                    d = input('Edit or remove a character in the generated password? (y/n) ')
+                    if d in ycho:
                         options(l)
                     sv = input('Do you want to save your password: (y/n) ')
-                    if sv in choices_y:
+                    if sv in ycho:
                         savefile(l)
                     print()
                     ch14 = input('Do you want to create a new password: (y/n) ').lower()
-                    if ch14 in choices_y:
+                    if ch14 in ycho:
                         main()
                     else:
                         print();options(l)
                     ch9 = input('Do you want to create a new password: (y/n) ').lower()
-                    if ch9 in choices_y:
+                    if ch9 in ycho:
                         main()
                     else:
                         print();options(l)
@@ -119,16 +119,16 @@ def main():
                 print()
                 print("Generated Password: ", "".join(l));print()
                 sv = input('Do you want to save your password: (y/n) ')
-                if sv in choices_y:
+                if sv in ycho:
                     savefile(l)
                 print()
                 ch10 = input('Do you want to create a new password: (y/n) ').lower()
-                if ch10 in choices_y:
+                if ch10 in ycho:
                     main()
                 else:
                     ch8 = input('Do you wanna go back to the previous options? (y/n) ').lower()
                     print()
-                    if ch8 in choices_y:
+                    if ch8 in ycho:
                         options(l)
                     else:
                         print('-----' * 25);main()
@@ -139,12 +139,12 @@ def main():
         print(data)
         t = input('Do you want to clear all saved passwords? (y/n)')
         print()
-        if t in choices_y:
+        if t in ycho:
             f = open('svdpswd.txt', 'a+')
             f.truncate(0);f.close()
             print('All saved passwords cleared.');print()
         s = input('Do you wanna return to the main menu? (y/n)')
-        if s in choices_y:
+        if s in ycho:
             main()
         else:
             exit()
